60630e93201ef20c1eb5ef3250145804f4bb9808
LCZ-ctrl <3415369092@qq.com>
2025-06-19 20:25:03 +0800

    Create defaults.py

---
+++ defaults.py
@@ -0,0 +1,11 @@
+DEFAULT_CONFIG = {
+    "mcts_simulations": 20,
+    "rollout_steps": 8,
+    "self_eval_weight": 0.5,
+    "kl_beta": 0.01,
+    "pref_threshold": 0.0,
+    "max_train_steps": 10000,
+    "batch_size": 4,
+    "learning_rate": 1e-5,
+    "save_checkpoint_steps": 2000,
+}
